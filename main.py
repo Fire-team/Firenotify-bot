c4649714337c7e003776c33161c329b9240b93bc
realayna <100434027+realayna@users.noreply.github.com>
2023-03-26 19:04:47 +0200

    improvements

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
-import telebot 
+import os
+from telebot import telebot
+from telebot import types
+from dotenv import load_dotenv
+from datetime import datetime
+from keyboard import *
+
+
+BOT_TOKEN=BOT_TOKEN = os.getenv('BOT_TOKEN')
+bot = telebot.TeleBot(BOT_TOKEN)
 
