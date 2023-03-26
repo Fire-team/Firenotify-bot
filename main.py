517ada18103d4b86982385f28c2769a808e2f0c7
realayna <100434027+realayna@users.noreply.github.com>
2023-03-26 19:33:41 +0200

    added simple commans

--- main.py
+++ main.py
@@ -5,7 +5,22 @@ from dotenv import load_dotenv
 from datetime import datetime
 from keyboard import *
 
+load_dotenv()
 
 BOT_TOKEN=BOT_TOKEN = os.getenv('BOT_TOKEN')
 bot = telebot.TeleBot(BOT_TOKEN)
+COMMAND_LIST = "HELP"
 
+
+
+
+#Message handlers
+@bot.message_handler(commands = ['start'])
+def send_welcome(message):
+    bot.send_message(message.chat.id, "Welcome to our FIRENOTIFY BOT!")
+@bot.message_handler(commands=['help'])
+def send_help(message):
+    bot.send_message(message.chat.id, COMMAND_LIST)
+
+
+bot.polling()
