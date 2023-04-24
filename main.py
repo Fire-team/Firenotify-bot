837405fd89e60cb4d0bd501ef53cffd4fb0bdaf8
realayna <100434027+realayna@users.noreply.github.com>
2023-04-24 07:46:42 +0200

    added loc and time

--- main.py
+++ main.py
@@ -21,11 +21,25 @@ def send_welcome(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     item1 = types.KeyboardButton('get news')
     item2 = types.KeyboardButton('get stats')
-    item3 = types.KeyboardButton('send location')
+    item3 = types.KeyboardButton('send location', request_location=True)
     item4 = types.KeyboardButton('send time')
-    item4 = types.KeyboardButton('get graph')
-    markup.add(item1, item2, item3, item4)
+    item5 = types.KeyboardButton('get graph')
+    markup.add(item1, item2, item3, item4, item5)
     bot.send_message(message.chat.id, "Welcome to our FIRENOTIFY BOT!", reply_markup=markup)
+
+
+@bot.message_handler(func=lambda message: True, content_types=['location'])
+def handle_location(message):
+    latitude = message.location.latitude
+    longitude = message.location.longitude
+    bot.send_message(message.chat.id, f"Your location is ({latitude}, {longitude}).")
+
+
+@bot.message_handler(func=lambda message: message.text == 'send time')
+def send_time(message):
+    now = datetime.now()
+    time_str = now.strftime("%H:%M:%S")
+    bot.send_message(message.chat.id, f"The current time is {time_str}.")
 
 
 @bot.message_handler(func=lambda message: message.text == 'get graph')
